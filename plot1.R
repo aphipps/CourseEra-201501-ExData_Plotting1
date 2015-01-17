f251ebda5fc8535f59a37e0013b7a45c182afb67
Alistair Phipps <git@alistairphipps.com>
2015-01-17 12:34:56 -0500

    Add missing plot1.R content

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data=read.table("household_power_consumption.txt",sep=";",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric"),header=TRUE,na.strings="?")
+data = subset(data, Date=="1/2/2007"|Date=="2/2/2007")
+data$DateTime = strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
+png(filename="plot1.png",width=480,height=480)
+hist(data$Global_active_power,col="Red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
